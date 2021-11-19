9586f3bd34566af0aa0200c50a51040ebda2b2e1
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2021-11-19 13:38:58 +0100

    completed seting up pulling data from google

---
+++ constants/index.js
@@ -0,0 +1,27 @@
+ const locations = [
+    "lagos",
+    "abuja",
+    "port harcourt",
+    "london",
+    "manchester city",
+    "dubai",
+    "new york",
+    "washington  dc",
+    "califonia",
+    "ontario",
+  ];
+  
+const  cousines = [
+    "rice",
+    "beans",
+    "egusi",
+    "chicken",
+    "salad",
+    "noodles",
+    "fried yam",
+    "scorge egg",
+    "chinese fries",
+    "fried fish",
+  ];
+
+  module.exports={locations, cousines}
--- contraollers/restaurantscontrollers.js
+++ contraollers/restaurantscontrollers.js
@@ -1,86 +1,38 @@
-const path = require("path")
-const csv = require('csv-parser')
-const fs = require('fs')
-const axios = require('axios')
+const path = require("path");
+const csv = require("csv-parser");
+const fs = require("fs");
 const results = [];
-//https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+toronto+canada&key=AIzaSyDycYHuYgJZxM3k2hBOGl_W8kg4_fshYNc
 
-//lagos
-//abuja
-//port harcourt
-//london
-//manchester+city
-//dubai
-//new+york
-//washington + dc
-//califonia
-//onterio
+//constants
+const { locations, cousines } = require("../constants/index");
 
-//jellof+rice
-//pounded+yam
-//egusi
-//
-let cousines=[
-  'rice',
-  'beans',
-  'egusi',
-  'chicken',
-  'salad',
-  'noodles',
-  'fried yam',
-  'scorge egg',
-  'chinese fries',
-  'fried fish'
-]
-let extendCousines=Array(10).fill(cousines).flat() 
+//HELPERS
+const { callFromGoogle } = require("../helpers/index");
 
-const getAllRestaurants=async(req, res)=>{
-let exactPath=path.dirname(__filename)
-fs.createReadStream(exactPath+"/"+"sample.csv")
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {    
-    res.status(200).json(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  });
-// console.log(req)
-}
+const getAllRestaurants = async (req, res) => {
+  let exactPath = path.dirname(__filename);
+  fs.createReadStream(exactPath + "/" + "sample.csv")
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      res.status(200).json(results);
+      // [
+      //   { NAME: 'Daffy Duck', AGE: '24' },
+      //   { NAME: 'Bugs Bunny', AGE: '22' }
+      // ]
+    });
+  // console.log(req)
+};
 
-const getAllRestaurantsByGoogle= async (req, res, next) => {
-  let resul=[]
+const getAllRestaurantsByGoogle = async (req, res, next) => {
   try {
-    const neighborhood = 'lekki'
-    const borough = 'epe'
-    const city = 'lagos'
-    const category = 'rice'
-    const key ='AIzaSyDycYHuYgJZxM3k2hBOGl_W8kg4_fshYNc'
-    const {data} = await axios.get(
-    
- `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${category}+${neighborhood}+${borough}+${city}&type=restaurant&key=${key}`
-    )
+   let restaurants= await callFromGoogle(locations, cousines)
+    .then((a) => a);
 
-  let a=  data.results.map((restaurant)=>{
-      return {
-        Name:restaurant.name,
-        image:restaurant.photos,
-        location:"lagos"
-      }
-    })
-// console.log(a)
-    for(let i=0; i<a.length;i++){
-      a[i].cousines=extendCousines[i]
-      resul.push(a[i])
-    }
-    res.json(resul)
+    res.status(200).json(restaurants)
+  } catch (err) {
+    next(err);
+  }
+};
 
-    } 
-  catch (err) {
-   next(err)
- }
- }
-
-
-module.exports={getAllRestaurants, getAllRestaurantsByGoogle}
+module.exports = { getAllRestaurants, getAllRestaurantsByGoogle };
